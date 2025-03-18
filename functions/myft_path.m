a50f745feed3788495d7f99df7291d65b1ec290c
rbruna <ricbruna@ucm.es>
2025-03-18 17:28:29 +0100

    Updated search folders.

--- functions/myft_path.m
+++ functions/myft_path.m
@@ -1,19 +1,34 @@
 function myft_path
 
-% Search for a version of FiedlTrip in the path.
-if ~isempty ( which ( 'ft_defaults' ) ), return, end
+% Search for a version of FieldTrip in the path.
+if isempty ( which ( 'ft_defaults' ) )
 
-% Looks for FieldTrip in the parent folder.
-ft_path = dir ( sprintf ( '%s/fieldtrip*', fileparts ( pwd ) ) );
-ft_path = ft_path ( [ ft_path.isdir ] );
+    % Looks for FieldTrip in the current folder.
+    ft_path = dir ( sprintf ( '%s/fieldtrip-*', pwd ) );
+    ft_path = ft_path ( [ ft_path.isdir ] );
 
-% Adds, if any version, FieldTrip to the path.
-if numel ( ft_path )
-    addpath ( sprintf ( '%s/%s/', fileparts ( pwd ), ft_path ( end ).name ) )
+    % If not found, looks for FieldTrip in the parent folder.
+    if ~numel ( ft_path )
+        ft_path = dir ( sprintf ( '%s/fieldtrip-*', fileparts ( pwd ) ) );
+        ft_path = ft_path ( [ ft_path.isdir ] );
+    end
     
-    % Otherwise exits with an error.
-else
-    error ( 'preproc:NoFT', 'No FieldTrip version neither in your path nor in the parent of the current folder.\nThis script cannot continue.' )
+    % If not found, rises an error.
+    if ~numel ( ft_path )
+        error ( ...
+            'preproc:NoFT', [ ...
+            'FieldTrip not found neither in your path, the current folder, nor its parent folder.\n' ...
+            'This script cannot continue.' ] )
+    end
+
+    % Otherwise adds FieldTrip to the path.
+    addpath ( sprintf ( '%s/%s/', ft_path ( end ).folder, ft_path ( end ).name ) )
 end
 
+% Initializes FieldTrip.
 ft_defaults
+
+% Disables FieldTrip feedback.
+global ft_default;
+ft_default.showcallinfo = 'no';
+ft_default.checkconfig  = 'silent';
